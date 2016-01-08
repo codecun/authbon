c4a29f51ffe4c50296376a1b4210d2d6f58f4f60
Humberto Santos <1201hs@gmail.com>
2016-01-08 12:58:45 -0500

    create basic auth in bonfire

--- src/Auth.php
+++ src/Auth.php
@@ -1,6 +1,8 @@
 <?php
 namespace codecun\authbon;
 
+use codecun\authbon\PasswordHash as CPass;
+
 
 /*==============================================================================================================================
  * @access public Class Auth with bonfire
@@ -13,22 +15,49 @@ class Auth
 {
     // --- ATTRIBUTES ---
     
-    private $_var            = null;
+    private $_cpass            = null;
 
     /*==============================================================================================================================
     * Autentificacion con el Objeto Header
     ==============================================================================================================================*/
-    public function __construct()
+    public function login($user, $password, $model)
     {
+        // Check whether the account has been banned.
+        if ($model->banned) {
+            return false;
+        }
 
+        // Check whether the username, email, or password doesn't exist.
+        if ($model == false) {
+            return false;
+        }
+
+        // Check whether the account has been soft deleted. The >= 1 check ensures
+        // this will still work if the deleted field is a UNIX timestamp.
+        if ($model->deleted >= 1) {
+            return false;
+        }
+
+        // Try password
+        if (! $this->check_password($password, $model->password_hash)) {
+            return false;
+        }
+
+        // Check whether the user needs to reset their password.
+        if ($model->force_password_reset == 1) {
+            return false;
+        }
+
+        return true;
     }
 
-    /*==============================================================================================================================
-    * Autentificacion con el Objeto Header
-    ==============================================================================================================================*/
-    public function auth(Header $header)
+    public function check_password($password, $hash)
     {
-        $this->_headers = $header->getHeader();
+        // Load the password hash library
+        $this->_cpass = new CPass(-1);
+
+        // Try password
+        return $this->_cpass->CheckPassword($password, $hash);
     }
     
 } 
