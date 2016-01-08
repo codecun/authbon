a14ef8ce9e6d32517e0f234a2ba7846096a8473c
Humberto Santos <1201hs@gmail.com>
2016-01-08 16:01:53 -0500

    report errors in auth bonfire hash

--- src/Auth.php
+++ src/Auth.php
@@ -14,6 +14,7 @@ class Auth
 {
     // --- ATTRIBUTES ---
     private $_cpass            = null;
+    private $_error            = null;
 
     /*==============================================================================================================================
     * Autentificacion con el Objeto Header
@@ -22,31 +23,41 @@ class Auth
     {
         // Check whether the account has been banned.
         if ($model->banned) {
+            $this->_error = "Check whether the account has been banned.";
             return false;
         }
 
         // Check whether the username, email, or password doesn't exist.
         if ($model == false) {
+            $this->_error = "Check whether the username, email, or password doesn't exist.";
             return false;
         }
 
         // Check whether the account has been soft deleted. The >= 1 check ensures
         // this will still work if the deleted field is a UNIX timestamp.
         if ($model->deleted >= 1) {
+            $this->_error = "Check whether the account has been soft deleted.";
             return false;
         }
 
         // Try password
         if (! $this->check_password($password, $model->password_hash)) {
+            $this->_error = "Incorrect Password";
             return false;
         }
 
         // Check whether the user needs to reset their password.
         if ($model->force_password_reset == 1) {
+            $this->_error = "Check whether the user needs to reset their password.";
             return false;
         }
 
         return true;
+    }
+
+    public function getError()
+    {
+        return $this->_error;
     }
 
     public function check_password($password, $hash)
